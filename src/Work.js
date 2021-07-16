39cde5930313d9fa4866beb49985df6e56794bc9
Burak <95burakbayar@gmail.com>
2021-07-16 04:26:23 +0300

    update

--- src/Work.js
+++ src/Work.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Deneme extends Component {
+export default class Table extends Component {
     constructor(props) {
         super(props);
      
